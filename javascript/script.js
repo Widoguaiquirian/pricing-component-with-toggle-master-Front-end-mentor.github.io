5d1a7d3806cda2829ce4b379469437f069d40baf
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-12-21 23:59:21 -0300

    Changing the JS code for :has()

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,1 @@
 "use strict";
-const toggleBtn = document.querySelector(".toggleBtn");
-const months = document.querySelectorAll(".month");
-const annually = document.querySelectorAll(".annually");
-
-toggleBtn.addEventListener("change", function () {
-   months.forEach(function (month) {
-      month.classList.toggle("show");
-      annually.forEach(function (anual) {
-         anual.classList.toggle("hide");
-      });
-   });
-});
